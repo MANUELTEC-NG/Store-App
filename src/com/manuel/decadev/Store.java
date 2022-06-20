1ffa0f3eccf066abccc4af0bf7bf42e827186b4e
Emmanuel Oladipupo <manueltech401@gmail.com>
2022-06-20 20:50:44 +0100

    setup the project package

---
+++ src/com/manuel/decadev/Store.java
@@ -0,0 +1,9 @@
+package com.manuel.decadev;
+
+public class Store {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to Our Store");
+    }
+}
