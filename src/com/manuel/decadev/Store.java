9702dc1a8d1224aedeef6476d269c507b69c8710
Emmanuel Oladipupo <manueltech401@gmail.com>
2022-06-24 08:02:24 +0100

    change the program logic in Store class

--- src/com/manuel/decadev/Store.java
+++ src/com/manuel/decadev/Store.java
@@ -18,31 +18,50 @@ public class Store {
 	// write your code
 
         Scanner scanner = new Scanner(System.in);
+        prompt();
 
-        System.out.println("What class would you like to interact with?");
-        System.out.println("For Manager Class \nPress -> 1\nFor Customer class -> Press 2\n" +
-                "Press Any key to quit\n");
-        int input = scanner.nextInt();
-        if (input == 1)
-            interactWithManagerClass();
-        else if (input == 2)
-            interactWithCahierClass();
-        else
-            System.out.println("Quiting...Goodbye!");
-            System.exit(0);
+            while (true) {
+
+                int input =  scanner.next().charAt(0);
+
+                if (input != '1' && input != '2' && input != 'Y') {
+                    System.out.println("Quiting...Goodbye!");
+                    System.exit(0);
+                }
+
+                if (input == '1') {
+                    interactWithManagerClass();
+
+                } else if (input == '2')
+                    interactWithCahierClass();
+
+
+                input = retry(scanner);
+
+            }
 
     }
 
+    public static void prompt(){
+        System.out.println("What class would you like to interact with?");
+        System.out.println("For Manager Class \nPress -> 1\nFor Customer class -> Press 2\n" +
+                "Press Any key to quit\n");
+
+    }
 
     public static void interactWithManagerClass(){
 
         Manager aManager = new Manager(2002,"Master", "Java",
                 "Engineering Department", "Manager");
+        char retry;
         System.out.println("Heads up for the Manager's Intro");
+
         PrintHandler.outputHelperMethod("..................................");
+
         String selfIntro = "My name Is" + " "+aManager.getFirstName()+
                 " "+ aManager.getLastName()+ "." + "A" +" "+aManager.getRole() +" "+"In the"+" "+aManager.getDepartment()+" "
                 + "In"+" " + Manager.getCompanyName();
+
         Cashier mary = new Cashier(1003, "Mary", "Daniel",
                 "Marketing","Cashier");
 
@@ -57,6 +76,7 @@ public class Store {
         //aManager.issueQuery(mary);
 
 
+
     }
 
     public static void interactWithCahierClass(){
@@ -64,5 +84,14 @@ public class Store {
                 "something@gmail.com", 234-421-494);
         customer.printReceipt();
 
+    }
+
+    public static int retry(Scanner scanner){
+        char retry;
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Would you want to have a RE-RUN?\n 'Y' for Yes & 'N' otherwise");
+        retry = scanner.next().charAt(0);
+        scanner.nextLine();
+     return retry;
     }
 }
